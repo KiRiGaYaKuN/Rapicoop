b3fbadc0af19afbc92c8d3ecc1ea6e059c980efc
= <=>
2022-09-14 13:45:06 -0500

    V3.0

--- app/src/main/java/com/example/rapicoop/LogIn.java
+++ app/src/main/java/com/example/rapicoop/LogIn.java
@@ -30,7 +30,7 @@ public class LogIn extends AppCompatActivity {
             @Override
             public void onClick(View view) {
 
-                Intent i = new Intent(LogIn.this, SigIn.class);
+                Intent i = new Intent(LogIn.this, Ofertarcomida.class);
                 startActivity(i);
             }
         });
